9dab65f37d5119514295735429bd1942940ad1a9
wade <wbb499477096@qq.com>
2017-06-19 18:47:41 +0800

    '1.anti-pattern'

--- components/Person.js
+++ components/Person.js
@@ -10,6 +10,12 @@ class Person  extends Component {
     static propTypes = {
         detail: PropTypes.object.isRequired
     };
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: {foo: 'bar'}
+        }
+    }
 
     componentWillMount() {
         console.log('person will mount');
@@ -18,6 +24,12 @@ class Person  extends Component {
     componentWillReceiveProps(newProps){
         console.log(`我新的props的name是${newProps.detail.name}，age是${newProps.detail.age}。我以前的props的name是${this.props.detail.name}，age是${this.props.detail.age}是我要re-render了`);
     }
+    handleOnclick() {
+        var value = this.state.value;
+        value.foo += 'bar'; // ANTI-PATTERN!
+        this.setState({ value: value });
+    }
+
     render() {
         console.log("我re-render了");
         const {name,age} = this.props.detail;
@@ -25,7 +37,8 @@ class Person  extends Component {
         return (
             <div>
                 <span>姓名:</span><span>{name}</span>
-                <span> age:</span><span>{age}</span>
+                <span onClick={this.handleOnclick.bind(this)}> age:</span><span>{age}</span>
+                <span>{this.state.value.foo}</span>
             </div>
         )
     }
