9fc6468937a933eac6896afaefd732ba95bb0fe1
wade <wbb499477096@qq.com>
2017-06-20 20:46:32 +0800

    1.use Map

--- components/Text.js
+++ components/Text.js
@@ -2,23 +2,28 @@
  * Created by topcmm on 6/20/2017.
  */
 import React, { Component } from 'react';
-import _ from 'lodash';
+import { Map } from 'immutable';
+//import _ from 'lodash';
 
 export default class extends Component {
     state = {
-        data: { times: 0}
+        data: Map({ times: 0 })
     };
 
     handleAdd() {
-        let data = _.cloneDeep(this.state.data);
-
+        /*//let data = _.cloneDeep(this.state.data);
+        let data = this.state.data;
         data.times = data.times + 1;
         this.setState({ data: data });
         // 如果上面不做 cloneDeep，下面打印的结果会是已经加 1 后的值。
-        console.log(this.state.data.times);
+        console.log(this.state.data.times);*/
+
+        this.setState({ data: this.state.data.update('times', v => v + 1) });
+        // 这时的 times 并不会改变
+        console.log(this.state.data.get('times'));
     }
 
     render() {
-        return (<span onClick={this.handleAdd.bind(this)}>add{this.state.data.times}</span>);
+        return (<span onClick={this.handleAdd.bind(this)}>add{this.state.data.get('times')}</span>);
     }
 }
